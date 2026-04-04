69b8f197dd9cc54dd9857ab62d1afbc35c72fa1a
Zebeyova <18089129639@163.com>
2026-04-04 18:28:23 +0800

    Patrol移动逻辑完成

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -73,7 +73,7 @@ namespace Script.Enemy
                 Distance = (_startPosition - gameObject.transform.position).normalized;
                 transform.Rotate(Vector3.up, Vector3.Cross(Distance, gameObject.transform.forward).y > 0 ? 180 : 0,
                     Space.Self); //左边
-                if (Vector3.Distance(_startPosition, gameObject.transform.position) < 0.5f)
+                if (Vector3.Distance(_startPosition, gameObject.transform.position) < endError)
                 {
                     _rb2dEnemy.velocity = Vector3.zero;
                     _eAnimationController.IdleAnimation();
@@ -87,13 +87,80 @@ namespace Script.Enemy
 
         private void PatrolMove()
         {
-            
+            Vector3 Distance;
+            if (_startPosition == Vector3.zero)
+            {
+                _startPosition = transform.position;
+                RandomBorder(false);
+            }
+
+            if (_eDetectionArea.GetDetectionArea())
+            {
+                _patrolDirection = 0;
+                Distance = _playerTransform.position - gameObject.transform.position;
+                transform.Rotate(Vector3.up, Vector3.Cross(Distance, gameObject.transform.forward).y > 0 ? 180 : 0,
+                    Space.Self); //左边
+                if (Distance.magnitude < 1.2f)
+                {
+                    _rb2dEnemy.velocity = Vector3.zero;
+                    _eAnimationController.AttackAnimation();
+                    return;
+                }
+
+                Distance = Distance.normalized;
+                _eAnimationController.WalkAnimation();
+                _rb2dEnemy.velocity = Distance * baseSpeed;
+            }
+            else
+            {
+                switch (_patrolDirection)
+                {
+                    case -1:
+                        Distance = _leftPatrolBorder - gameObject.transform.position;
+                        if (Distance.magnitude < endError) _patrolDirection = 1;
+                        break;
+                    case 1:
+                        Distance = _rightPatrolBorder - gameObject.transform.position;
+                        if (Distance.magnitude < endError) _patrolDirection = -1;
+                        break;
+                    default:
+                        Distance = _startPosition - gameObject.transform.position;
+                        transform.Rotate(Vector3.up,
+                            Vector3.Cross(Distance, gameObject.transform.forward).y > 0 ? 180 : 0,
+                            Space.Self);
+                        Distance = Distance.normalized;
+                        _rb2dEnemy.velocity = Distance * baseSpeed;
+                        if (Vector3.Distance(_startPosition, gameObject.transform.position) < endError)
+                            RandomBorder(true);
+                        return;
+                }
+                transform.Rotate(Vector3.up, Vector3.Cross(Distance, gameObject.transform.forward).y > 0 ? 180 : 0,
+                    Space.Self);
+                Distance = Distance.normalized;
+                _rb2dEnemy.velocity = Distance * baseSpeed;
+            }
+        }
+
+        private void RandomBorder(bool reset)
+        {
+            //TODO:需要修改
+            var RandomBorderNum = Random.Range(0f, endError);
+            _patrolDirection = Random.Range(0, 2) == 0 ? -1 : 1;
+
+            var RandomVector = new Vector3(RandomBorderNum, 0, 0);
+            _leftPatrolBorder = _startPosition - transform.right * 2 - RandomVector;
+            _rightPatrolBorder = _startPosition + transform.right * 2 + RandomVector;
         }
 
         #region 属性
 
         public float baseSpeed = 0.5f;
+        public float endError = 0.5f;
         private Vector3 _startPosition;
+
+        private Vector3 _leftPatrolBorder; //左边界
+        private Vector3 _rightPatrolBorder; //右边界
+        private int _patrolDirection; //巡逻方向
 
         #endregion
 
